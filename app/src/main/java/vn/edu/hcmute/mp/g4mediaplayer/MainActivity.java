3df2d1e017ef4ca75105d0c0be7244612c38bce7
Nguyen Le Dien <ngdien1998@gmail.com>
2019-03-27 21:52:30 +0700

    Initial commit

---
+++ app/src/main/java/vn/edu/hcmute/mp/g4mediaplayer/MainActivity.java
@@ -0,0 +1,13 @@
+package vn.edu.hcmute.mp.g4mediaplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
