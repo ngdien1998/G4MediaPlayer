f479a7c27a6f35e2ec40505663852157984b97b4
PTIT71 <40396579+PTIT71@users.noreply.github.com>
2019-05-15 22:40:59 +0700

    PLaylist show

--- app/src/main/java/vn/edu/hcmute/mp/g4mediaplayer/activity/PlaylistSongActivity.java
+++ app/src/main/java/vn/edu/hcmute/mp/g4mediaplayer/activity/PlaylistSongActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
@@ -16,6 +18,7 @@ import vn.edu.hcmute.mp.g4mediaplayer.R;
 import vn.edu.hcmute.mp.g4mediaplayer.adapter.SongAdapter;
 import vn.edu.hcmute.mp.g4mediaplayer.adapter.SongPlaylistAdapter;
 import vn.edu.hcmute.mp.g4mediaplayer.common.Consts;
+import vn.edu.hcmute.mp.g4mediaplayer.common.Helper;
 import vn.edu.hcmute.mp.g4mediaplayer.model.entity.PlayList;
 import vn.edu.hcmute.mp.g4mediaplayer.model.entity.Song;
 import vn.edu.hcmute.mp.g4mediaplayer.model.service.ArtistService;
@@ -24,7 +27,7 @@ import vn.edu.hcmute.mp.g4mediaplayer.model.service.PlaylistService;
 public class PlaylistSongActivity extends AppCompatActivity {
     private TextView txtNamePlaylist;
     private RecyclerView rclSongPlaylist;
-    private ArrayList<Song> playLists;
+    private ArrayList<Song> songs;
     private SongPlaylistAdapter adapter;
     private  PlaylistService  service;
     @Override
@@ -40,13 +43,32 @@ public class PlaylistSongActivity extends AppCompatActivity {
 
         try {
             service = new PlaylistService(getApplicationContext());
-            playLists = service.getSongList(playList.getId());
+            songs = service.getSongList(playList.getId());
 
-            adapter = new SongPlaylistAdapter(getApplicationContext(), playLists);
+            adapter = new SongPlaylistAdapter(getApplicationContext(), songs);
+            adapter.setOnMoreItemClick(this::adapterClicckMore);
+            adapter.setOnItemClick(this::adapterOnItemClick);
 
             rclSongPlaylist.setAdapter(adapter);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void adapterClicckMore(View view, Song song, int position, MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.action_play:
+                Helper.showToast(getApplicationContext(), "klgjdfkgjklfdhjkfdhjkfdhjlkfdj");
+                break;
+        }
+    }
+
+    private void adapterOnItemClick(View view, Song song, int position) {
+        Intent intent = new Intent(getApplicationContext(), PlayCenterActivity.class);
+        intent.putExtra(Consts.SONGS_EXTRA, songs);
+        intent.putExtra(Consts.SONG_EXTRA, song);
+        intent.putExtra(Consts.SONG_POSITION_EXTRA, position);
+
+        startActivity(intent);
     }
 }
